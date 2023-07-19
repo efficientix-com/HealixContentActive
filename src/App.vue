4d80164d69cbe9d0bdbbb70759cc5070c6967b42
MagdielJT <magdiel.jimenez@freebug.mx>
2023-07-19 12:04:22 -0600

    Com

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Healix content active"/>
+    <HelloWorld msg="Healix content active2"/>
   </div>
 </template>
 
